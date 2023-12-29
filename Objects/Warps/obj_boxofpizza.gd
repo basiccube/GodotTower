8952bab77b26a0c35bdafee8f03e2ad0b92f0db3
basiccube <67266535+basiccube@users.noreply.github.com>
2023-12-29 16:59:28 +0200

    entrance_6 fix

--- Objects/Warps/obj_boxofpizza.gd
+++ Objects/Warps/obj_boxofpizza.gd
@@ -32,6 +32,7 @@ func _process(delta):
 			obj_player.targetRoom = targetRoom
 			obj_player.set_animation("downpizzabox")
 			obj_player.state = global.states.door
+			z_index = 10
 	if (scale.y == -1):
 		if ((Input.is_action_pressed("key_up") && (obj_player.state == global.states.normal || obj_player.state == global.states.jump || obj_player.state == global.states.mach1 || obj_player.state == global.states.mach2 || obj_player.state == global.states.mach3 || obj_player.state == global.states.Sjumpprep || obj_player.state == global.states.Sjump) || obj_player.state == global.states.Sjumpland) && $PlayerArea.overlaps_body(obj_player) && (!utils.instance_exists("obj_fadeout")) && obj_player.state != global.states.door && obj_player.state != global.states.comingoutdoor):
 			utils.playsound("Box")
@@ -45,3 +46,4 @@ func _process(delta):
 			obj_player.targetRoom = targetRoom
 			obj_player.set_animation("uppizzabox")
 			obj_player.state = global.states.door
+			z_index = 10
